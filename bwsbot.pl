60c695778b4c5d1c45e9f55e5f2f68816aa27934
Kyle M Hall <kyle@bywatersolutions.com>
2019-07-26 08:45:40 -0400

    Tidy bot script

--- bwsbot.pl
+++ bwsbot.pl
@@ -13,16 +13,16 @@ use Slack::RTM::Bot;
 use YAML::XS qw(LoadFile DumpFile);
 
 my $slack_bot_token = $ENV{SLACK_BOT_TOKEN};
-my $data_file = $ENV{DATA_FILE};
-my $debug = 1;
+my $data_file       = $ENV{DATA_FILE};
+my $debug           = 1;
 
 say "BWSBot is starting!" if $debug;
 
 die "No SLACK_BOT_TOKEN set!" unless $slack_bot_token;
-die "No DATA_FILE set!" unless $data_file;
+die "No DATA_FILE set!"       unless $data_file;
 
 my $data = LoadFile($data_file);
-unless ( $data ) {
+unless ($data) {
     $data = { agendas => {} };
     DumpFile( $data_file, $data );
 }
@@ -39,7 +39,7 @@ my $bot = Slack::RTM::Bot->new( token => $slack_bot_token );
 
 =cut
 
-my $regex_rt     = qr/(ticket|rt)\s*([0-9]+)/mi;
+my $regex_rt              = qr/(ticket|rt)\s*([0-9]+)/mi;
 my $handle_ticket_numbers = sub {
     my ($response) = @_;
     warn "handle_ticket_numbers" if $debug;
@@ -70,7 +70,7 @@ $bot->on( { text => $regex_rt }, $handle_ticket_numbers );
 
 =cut
 
-my $regex_bz     = qr/(bug|bz)\s*([0-9]+)/mi;
+my $regex_bz           = qr/(bug|bz)\s*([0-9]+)/mi;
 my $handle_bug_numbers = sub {
     my ($response) = @_;
     warn "handle_bug_numbers" if $debug;
@@ -97,7 +97,7 @@ $bot->on( { text => $regex_bz }, $handle_bug_numbers );
 
 =cut
 
-my $regex_coffee = qr/(coffee)\s*([0-9]+)/mi;
+my $regex_coffee  = qr/(coffee)\s*([0-9]+)/mi;
 my $handle_coffee = sub {
     my ($response) = @_;
     warn "handle_coffee" if $debug;
@@ -131,7 +131,7 @@ my $handle_coffee = sub {
 };
 $bot->on( { text => $regex_coffee }, $handle_coffee );
 
-my $regex_agenda_help = qr/(agenda help)\s*/mi;
+my $regex_agenda_help  = qr/(agenda help)\s*/mi;
 my $handle_agenda_help = sub {
     my ($response) = @_;
     warn "handle_agenda_help" if $debug;
@@ -154,7 +154,7 @@ my $handle_agenda_help = sub {
 };
 $bot->on( { text => $regex_agenda_help }, $handle_agenda_help );
 
-my $regex_agenda_create = qr/(agenda create)\s*(.+)/mi;
+my $regex_agenda_create  = qr/(agenda create)\s*(.+)/mi;
 my $handle_agenda_create = sub {
     my ($response) = @_;
     warn "handle_agenda_create" if $debug;
@@ -169,7 +169,8 @@ my $handle_agenda_create = sub {
     my $text;
     if ( $data->{agendas}->{$agenda} ) {
         $text = "The agenda `$agenda` already exists";
-    } else {
+    }
+    else {
         $data->{agendas}->{$agenda} = {};
         $text = "The agenda `$agenda` has been created";
         DumpFile( $data_file, $data );
@@ -182,7 +183,7 @@ my $handle_agenda_create = sub {
 };
 $bot->on( { text => $regex_agenda_create }, $handle_agenda_create );
 
-my $regex_agenda_destroy = qr/(agenda destroy)\s*(.+)/mi;
+my $regex_agenda_destroy  = qr/(agenda destroy)\s*(.+)/mi;
 my $handle_agenda_destroy = sub {
     my ($response) = @_;
     warn "handle_agenda_destroy" if $debug;
@@ -195,9 +196,10 @@ my $handle_agenda_destroy = sub {
     $agenda =~ s/^\s+|\s+$//g;
 
     my $text;
-    if ( ! exists $data->{agendas}->{$agenda} ) {
+    if ( !exists $data->{agendas}->{$agenda} ) {
         $text = "The agenda `$agenda` doesn't exist";
-    } else {
+    }
+    else {
         delete $data->{agendas}->{$agenda};
         $text = "The agenda `$agenda` has been destroyed";
         DumpFile( $data_file, $data );
@@ -210,7 +212,7 @@ my $handle_agenda_destroy = sub {
 };
 $bot->on( { text => $regex_agenda_destroy }, $handle_agenda_destroy );
 
-my $regex_agenda_list = qr/(agenda list)\s*/mi;
+my $regex_agenda_list  = qr/(agenda list)\s*/mi;
 my $handle_agenda_list = sub {
     my ($response) = @_;
     warn "handle_agenda_list" if $debug;
@@ -220,11 +222,12 @@ my $handle_agenda_list = sub {
     my @agendas = $data->{agendas} ? keys %{ $data->{agendas} } : undef;
 
     my $text;
-    if ( @agendas ) {
+    if (@agendas) {
         $text = "Agendas:\n";
         my $i = 1;
         map { $text .= $i++ . ". $_\n" } @agendas;
-    } else {
+    }
+    else {
         $text = "There are no agendas at this time";
     }
 
@@ -235,7 +238,7 @@ my $handle_agenda_list = sub {
 };
 $bot->on( { text => $regex_agenda_list }, $handle_agenda_list );
 
-my $regex_agenda_add_item = qr/(agenda)\s*(.+)(add)\s*(.+)/mi;
+my $regex_agenda_add_item  = qr/(agenda)\s*(.+)(add)\s*(.+)/mi;
 my $handle_agenda_add_item = sub {
     my ($response) = @_;
     warn "handle_agenda_add_item" if $debug;
@@ -244,19 +247,21 @@ my $handle_agenda_add_item = sub {
 
     $response->{text} =~ $regex_agenda_add_item;
     my $agenda = $2;
-    my $item = $4;
+    my $item   = $4;
 
     $agenda =~ s/^\s+|\s+$//g;
-    $item =~ s/^\s+|\s+$//g;
+    $item   =~ s/^\s+|\s+$//g;
 
     my $text;
-    if ( ! exists $data->{agendas}->{$agenda} ) {
+    if ( !exists $data->{agendas}->{$agenda} ) {
         $text = "The agenda `$agenda` doesn't exist";
-    } else {
+    }
+    else {
         if ( ref $data->{agendas}->{$agenda}->{items} eq 'ARRAY' ) {
             push( @{ $data->{agendas}->{$agenda}->{items} }, $item );
-        } else {
-            $data->{agendas}->{$agenda}->{items} = [ $item ];
+        }
+        else {
+            $data->{agendas}->{$agenda}->{items} = [$item];
         }
 
         $text = "The item `$item` has been added to the agenda `$agenda`";
@@ -270,7 +275,7 @@ my $handle_agenda_add_item = sub {
 };
 $bot->on( { text => $regex_agenda_add_item }, $handle_agenda_add_item );
 
-my $regex_agenda_del_item = qr/(agenda)\s*(.+)(del)\s*([0-9]+)/mi;
+my $regex_agenda_del_item  = qr/(agenda)\s*(.+)(del)\s*([0-9]+)/mi;
 my $handle_agenda_del_item = sub {
     my ($response) = @_;
     warn "handle_agenda_del_item" if $debug;
@@ -279,21 +284,26 @@ my $handle_agenda_del_item = sub {
 
     $response->{text} =~ $regex_agenda_del_item;
     my $agenda = $2;
-    my $index = $4;
+    my $index  = $4;
 
     $agenda =~ s/^\s+|\s+$//g;
-    $index =~ s/^\s+|\s+$//g;
+    $index  =~ s/^\s+|\s+$//g;
 
-    $index--; # Because programmers count from zero ;)
+    $index--;    # Because programmers count from zero ;)
 
-    my @items = $data->{agendas}->{$agenda}->{items} ? @{ $data->{agendas}->{$agenda}->{items} } : undef;
+    my @items =
+      $data->{agendas}->{$agenda}->{items}
+      ? @{ $data->{agendas}->{$agenda}->{items} }
+      : undef;
 
     my $text;
-    if ( ! exists $data->{agendas}->{$agenda} ) {
+    if ( !exists $data->{agendas}->{$agenda} ) {
         $text = "The agenda `$agenda` doesn't exist";
-    } elsif ( ! exists $items[$index] ) {
+    }
+    elsif ( !exists $items[$index] ) {
         $text = "The agenda `$agenda` doesn't have an item at that index";
-    } else {
+    }
+    else {
         my $item = $items[$index];
         splice @items, $index, 1;
         $data->{agendas}->{$agenda}->{items} = \@items;
@@ -308,7 +318,7 @@ my $handle_agenda_del_item = sub {
 };
 $bot->on( { text => $regex_agenda_del_item }, $handle_agenda_del_item );
 
-my $regex_agenda_list_items = qr/(agenda)\s+(.+)\s+(list)\s*/mi;
+my $regex_agenda_list_items  = qr/(agenda)\s+(.+)\s+(list)\s*/mi;
 my $handle_agenda_list_items = sub {
     my ($response) = @_;
     warn "handle_agenda_list_items" if $debug;
@@ -320,14 +330,18 @@ my $handle_agenda_list_items = sub {
 
     $agenda =~ s/^\s+|\s+$//g;
 
-    my @items = $data->{agendas}->{$agenda}->{items} ? @{ $data->{agendas}->{$agenda}->{items} } : undef;
+    my @items =
+      $data->{agendas}->{$agenda}->{items}
+      ? @{ $data->{agendas}->{$agenda}->{items} }
+      : undef;
 
     my $text;
-    if ( @items ) {
+    if (@items) {
         $text = "Agenda `$agenda` items:\n";
         my $i = 1;
         map { $text .= $i++ . ". $_\n" } @items;
-    } else {
+    }
+    else {
         $text = "The agenda `$agenda` has no items at this time";
     }
 
@@ -341,10 +355,10 @@ $bot->on( { text => $regex_agenda_list_items }, $handle_agenda_list_items );
 $bot->start_RTM(
     sub {
 
-#        $bot->say(
-#            channel => 'general',
-#            text    => 'bwsbot at your service!',
-#        );
+        $bot->say(
+            channel => 'general',
+            text    => 'bwsbot at your service!',
+        );
 
         my $step = 1;
         while (1) {
